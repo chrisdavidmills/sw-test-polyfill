b8f2fe6b67bbd48db21696e9d80dd1d2b44388c2
Chris Mills <cmills@opera.com>
2014-07-22 15:39:07 +0100

    adding in polyfill code

--- sw.js
+++ sw.js
@@ -1,54 +1,80 @@
-this.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.create('v1').then(function(cache) {
-      return cache.add(
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
-      );
-    })
+"use strict";
+
+importScripts("polyfills/idbCacheUtils.js");
+importScripts("polyfills/fetchPolyfill.js");
+importScripts("polyfills/idbCachePolyfill.js");
+importScripts("polyfills/idbCacheStoragePolyfill.js");
+
+var log = console.log.bind(console);
+var err = console.error.bind(console);
+this.onerror = err;
+
+var notify = function() {
+  if (self.Notification && self.Notification.permission == "granted") {
+    new self.Notification(arguments[0]);
+  } else {
+    log.apply(arguments)
+  }
+};
+
+var baseUrl = (new URL("./", this.location.href) + "");
+notify(baseUrl);
+
+this.addEventListener("install", function(e) {
+  e.waitUntil(caches.create("v1").then(function(v1) {
+    var resourceUrls = [
+      "",
+      "?offline",
+      '/sw-test/',
+      '/sw-test/index.html',
+      '/sw-test/style.css',
+      '/sw-test/app.js',
+      '/sw-test/image-list.js',
+      '/sw-test/star-wars-logo.jpg',
+      '/sw-test/gallery/',
+      '/sw-test/gallery/bountyHunters.jpg',
+      '/sw-test/gallery/myLittleVader.jpg',
+      '/sw-test/gallery/snowTroopers.jpg'
+    ];
+
+    return Promise.all(resourceUrls.map(function(relativeUrl) {
+      return v1.add(baseUrl + relativeUrl);
+    }));
+  }));
+});
+
+this.addEventListener("fetch", function(e) {
+  var request = e.request;
+
+  if (this.scope.indexOf(request.origin) == -1) {
+    return;
+  }
+
+  // Basic read-through caching.
+  e.respondWith(
+    caches.match(request, "v1").then(
+      function(response) {
+        return response;
+      },
+      function() {
+        // we didn't have it in the cache, so add it to the cache and return it
+        return caches.get("v1").then(
+          function(v1) {
+            log("runtime caching:", request.url);
+
+            // FIXME(slighltyoff): add should take/return an array
+            return v1.add(request).then(
+              function(response) {
+                return response;
+              }
+            );
+          }
+        );
+      }
+    )
   );
 });
 
-// You could also do
-
-// this.addEventListener('install', function(event) {
-//   var starWarsAssets = new Cache();
-
-//   event.waitUntil(
-//     starWarsAssets.add(
-//       '/sw-test/index.html',
-//       '/sw-test/style.css',
-//       '/sw-test/app.js',
-//       '/sw-test/image-list.js',
-//       '/sw-test/star-wars-logo.jpg',
-//       '/sw-test/gallery/',
-//       '/sw-test/gallery/bountyHunters.jpg',
-//       '/sw-test/gallery/myLittleVader.jpg',
-//       '/sw-test/gallery/snowTroopers.jpg'
-//     )
-//   )
-
-//   caches.set('v1', starWarsResources);
-
-// )};
-
-this.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).catch(function() {
-      return event.default();
-      return caches.get('v1').then(function(cache) {
-        cache.add(event.request);
-      });
-    }).catch(function() {
-      return caches.match('/sw-test/gallery/myLittleVader.jpg');
-    })
-  );
+this.addEventListener("sync", function(e) {
+  this.clients.getServiced().then(log, err);
 });
